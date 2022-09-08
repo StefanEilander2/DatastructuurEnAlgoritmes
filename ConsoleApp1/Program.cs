5e3f9553237caa9e9ab1f1dad37d1ce27729a128
StefanEilander2 <s1157160@student.windesheim.nl>
2022-09-08 14:38:55 +0200

    epic opdracht ongekend mooi
    heel leuk

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp1;
+
+int[] Array = { -2, 11, -4, 13, -5, 2 };
+
+int MSCC(int[] Array)
+{
+    int Max = 0;
+    int sum = 0;
+
+    for (int i = 0; i < Array.Length; i++)
+    {
+        for (int j = i; j < Array.Length; j++)
+        {
+            int thisSum = 0;
+            for (int k = i; k <= j; k++)
+            {
+                thisSum += Array[k];
+            }
+
+            if (thisSum > Max)
+            {
+                Max = thisSum;
+            }
+        }
+    }
+
+    return Max;
+}
+
+Console.WriteLine(MSCC(Array));
---
+++ ConsoleApp1/SieveOfEratosthene.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp1;
+
+public class SieveOfEratosthene
+{
+    private List<int> GenerateList(int MaxNumber)
+    {
+        List<int> list = new List<int>();
+
+        for (int i = 2; i < MaxNumber; i++)
+        {
+            list.Add(i);
+        }
+
+        return list;
+    }
+
+    public List<int> GetPrimes(int maxNumber)
+    {
+        List<int> numbers = GenerateList(maxNumber);
+        List<int> Primes = new List<int>();
+        int p = 2;
+        while(p < maxNumber)
+        {
+            for (int i = 1; i < maxNumber; i++)
+            {
+                if (i * p != p)
+                {
+                    numbers.Remove(i * p);
+                }
+            }
+            Primes.Add(p);
+            numbers.Remove(p);
+            if (numbers.Count != 0)
+            {
+                p = numbers.Min();
+            }
+            else
+            {
+                p = maxNumber + 1;
+            }
+        }
+
+        return Primes;
+    }
+}
+
